d23159e6bd798f142236cb3bd439df9dfe1f02bc
Tenzin-whitehatjr <72287867+Tenzin-whitehatjr@users.noreply.github.com>
2020-11-16 23:07:43 -0500

    ye
    ye

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var bullet, thickness;
+var speed, wall, weight;
+function setup() {
+  createCanvas(1600,400);
+  thickness=random(22,83);
+  speed=random(223,321);
+  weight=random(30,52);
+
+  bullet = createSprite(400, 200, 50, 5);
+  bullet.velocityX = speed;
+  bullet.shapeColor="white";
+  
+
+  wall = createSprite(1200,200,thickness,height/2);
+  wall.shapeColor=color(80,80,80);
+  
+}
+
+function draw() {
+  background(255,255,255);
+  background("blue")
+  if(wall.x-bullet.x < (bullet.width+wall.width)/2) {
+    bullet.velocityX=0;
+    var deformation=0.5 * weight * speed* speed/(thickness *thickness *thickness);
+    if(deformation>10)
+    {
+      wall.shapeColor=color(255,0,0);
+    }
+    if(deformation<10)
+    {
+      wall.shapeColor=color(0,255,0);
+    }
+  }
+  
+ 
+  drawSprites();
+}
+
